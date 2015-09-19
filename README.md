f2a98e84455dbf455e664aaed21d1728d811bbdf
Per Arneng <per.arneng@scalebit.com>
2015-09-19 23:59:52 +0200

    improved help and fault tollerance

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ This is just a code to test the multiprocessing capabillities in python
 
 # Running
 ```
-$ python worcount.py txtfiles
+$ python worcount.py -h
+wordcount <opts> <directories>
+
+OPTIONS:
+ -m     use multiprocessing (optional)
+ -h     print this help (optional)
 ```
--- wordcount.py
+++ wordcount.py
@@ -2,26 +2,61 @@ __author__ = 'pure'
 import sys
 import wcutils
 import multiprocessing
+import datetime
 from multiprocessing import Pool
+
+def print_help():
+    print "wordcount <opts> <directories>"
+    print ""
+    print "OPTIONS:"
+    print " -m     use multiprocessing (optional)"
+    print " -h     print this help (optional)"
+    sys.exit(0)
 
 def main():
 
-    if len(sys.argv) < 2:
-        print("need folders with txt files as arguments")
+    if len(sys.argv) == 1:
+        print_help()
+        sys.exit(1)
+
+    args = sys.argv[1:]
+    pool = None
+
+    if "-h" in args:
+        print_help()
+        sys.exit(0)
+
+    if "-m" in args:
+        cpu_count = multiprocessing.cpu_count()
+        pool = Pool(cpu_count)
+        print "using multiprocessing with %s cpu's" % (cpu_count)
+        args.remove("-m")
+    else:
+        print "no multiprocessing"
+
+    if len(args) == 0:
+        print "error: missing directories as arguments"
+        print_help()
         sys.exit(1)
 
     all_results = []
 
-    cpu_count = multiprocessing.cpu_count()
-    pool = Pool(cpu_count)
-
-    for dir in sys.argv[1:]:
+    before = datetime.datetime.now()
+    for dir in args:
         results = wcutils.process_directory(dir, pool)
         for result in results:
             all_results.append(result)
+    delta = datetime.datetime.now() - before
+
+    total_lines = 0
+    total_words = 0
 
     for result in all_results:
-        print result
+        total_lines = total_lines + result.line_count
+        total_words = total_words + result.word_count
+
+    print "files: %s, lines: %s, words: %s" % (len(all_results), total_lines, total_words)
+    print "elapsed time: %s" % (delta)
 
 if __name__ == "__main__":
     main()
